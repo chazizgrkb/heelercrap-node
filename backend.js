495fcedc79e387d3661e57690fd1bfafd6933aa6
Chaz "Gamerappa" Péloquin <chazpelo2@gmail.com>
2022-06-24 23:29:54 -0400

    Revert "broken auth code"
    quite frankly it fucking sucked, reimplementing this in another way

--- backend.js
+++ backend.js
@@ -1,18 +1,5 @@
-const bcrypt = require('bcrypt');
-const db = require('./database');
-
-async function authenticate(name, pass) {
-	try {
-	const queryResult = await db.result('SELECT `password` FROM `users` WHERE `name` = ?', name);
-	const hash = queryResult[0].password;
-	const finalHash = hash.replace('$2y$', '$2a$');
-	const validPassword = bcrypt.compare(pass, finalHash, function(err, result) {
-		console.log("password is " + result);
-		return result;
-	});
-	} catch (err) {
-    console.log(err);
-	}
+function hello(name) {
+     console.log("hello " + name);
 }
 
-module.exports = {authenticate};
+module.exports = {hello};
--- http-server.js
+++ http-server.js
@@ -13,13 +13,13 @@ var options = {
 };
 
 app = express()
-app.get('/rdr/pprdr.asp', async (req, res) => {
+app.get('/rdr/pprdr.asp', (req, res) => {
   res.set("PassportURLs", "DALogin=login.passport.com/login2.srf")
   console.log("Tweener Nexus Request Start?")
   res.end()
 })
 
-app.get('/login2.srf', async (req, res) => {
+app.get('/login2.srf', (req, res) => {
   console.log("Tweener Nexus Login Request")
   res.set("PPServer", "H: LAWPPLOG5C006")
   res.set("P3P", 'CP="DSP CUR OTPi IND OTRi ONL FIN"')
@@ -28,27 +28,15 @@ app.get('/login2.srf', async (req, res) => {
   //res.end()
 })
 
-app.get('/login2_part2.srf', async (req, res) => {
+app.get('/login2_part2.srf', (req, res) => {
   var header1 = req.headers.authorization.split(/\s*,\s*/);
-  var username = header1[2].split('=')[1].split('@')[0]; // ass code
   var pass = header1[3].split('=')[1]; // ass code
+  console.log(pass);
   console.log("Tweener Nexus Login Request 2")
-	res.set("PPServer", "H: LAWPPIIS6B061")
-	res.set("P3P", 'CP="DSP CUR OTPi IND OTRi ONL FIN"')
-	const asyncExample = async () =>{
-		try {
-		//const fuck = await backend.authenticate(username, pass);
-		if (await backend.authenticate(username, pass) === true) {	
-			res.set("Authentication-Info", "Passport1.4 da-status=success,tname=MSPAuth,tname=MSPProf,tname=MSPSec,from-PP='t=53*1hAu8ADuD3TEwdXoOMi08sD*2!cMrntTwVMTjoB3p6stWTqzbkKZPVQzA5NOt19SLI60PY!b8K4YhC!Ooo5ug$$&p=5eKBBC!yBH6ex5mftp!a9DrSb0B3hU8aqAWpaPn07iCGBw5akemiWSd7t2ot!okPvIR!Wqk!MKvi1IMpxfhkao9wpxlMWYAZ!DqRfACmyQGG112Bp9xrk04!BVBUa9*H9mJLoWw39m63YQRE1yHnYNv08nyz43D3OnMcaCoeSaEHVM7LpR*LWDme29qq2X3j8N',ru=h")
-		} else {
-			res.set("WWW-Authenticate", "Passport1.4 da-status=failed,srealm=Passport.NET,ts=-3,prompt,cburl=http//www.passportimages.com/XPPassportLogo.gif,cbtxt=Type%20your%20e-mail%20address%20and%20password%20correctly.%20If%20you%20haven%E2%80%99t%20registered%20with%20.NET%20Passport%2C%20click%20the%20Get%20a%20.NET%20Passport%20link.")
-		}
-			res.end()
-		} catch (err) {
-		console.log(err);
-		}
-	}
-	asyncExample();
+  res.set("PPServer", "H: LAWPPIIS6B061")
+  res.set("P3P", 'CP="DSP CUR OTPi IND OTRi ONL FIN"')
+  res.set("Authentication-Info", "Authentication-Info: Passport1.4 da-status=success,tname=MSPAuth,tname=MSPProf,tname=MSPSec,from-PP='t=53*1hAu8ADuD3TEwdXoOMi08sD*2!cMrntTwVMTjoB3p6stWTqzbkKZPVQzA5NOt19SLI60PY!b8K4YhC!Ooo5ug$$&p=5eKBBC!yBH6ex5mftp!a9DrSb0B3hU8aqAWpaPn07iCGBw5akemiWSd7t2ot!okPvIR!Wqk!MKvi1IMpxfhkao9wpxlMWYAZ!DqRfACmyQGG112Bp9xrk04!BVBUa9*H9mJLoWw39m63YQRE1yHnYNv08nyz43D3OnMcaCoeSaEHVM7LpR*LWDme29qq2X3j8N',ru=h")
+  res.end()
 })
 
 var start = function(port,host) {
